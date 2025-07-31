9e17a807898d5547a58dfc39e38eb5520803262b
testing-user-prog <muhammadzaidbinsaad@gmail.com>
2025-08-01 02:17:03 +0500

    added index.html

---
+++ script.js
@@ -0,0 +1,58 @@
+let answer=false;
+let dis=document.getElementById("display");
+let exp="";
+function changedisplay(char)
+{
+    if(answer)
+    {
+        removedata();
+        answer=false;
+    
+    }
+    exp=dis.value+char;
+    dis.value=exp;
+
+
+}
+function operate()
+{
+    if(dis.value.toString().includes("="))
+    {
+        let a=dis.value.substring(1);
+        removedata();
+        exp=a;
+        dis.value=a;
+        answer=false;
+    }
+    else{
+ 
+       try
+       {
+        
+            
+            if(exp!="")
+            {
+                let y=eval(exp);
+                dis.value="="+y;
+            }
+     
+            
+        
+       }
+    catch(error)
+    {
+        
+        dis.value=error;
+
+
+    }
+    answer=true;
+    }
+    
+}
+function removedata()
+{
+    exp="";
+    dis.value=exp;
+
+}
